72545e10dcc2cb74808380fa263727448a52509d
yasinbec <yasinbec@gmail.com>
2014-11-01 21:52:13 -0400

    Files

---
+++ flashlight.js
@@ -0,0 +1,21 @@
+// FlashLight v1.0.0 for jQuery
+// =============
+// Author: Ali Beceren
+// Created: 2/14/2011
+// Date: 10/08/2014
+// Website: http://www.jenaldesign.com
+// Description: Another kind Lightbox
+
+(function($) {
+	
+	$.fn.flashlight = function() {		
+		
+		
+		$(this).addClass( "flashlight" );
+
+		
+
+			
+	};
+
+}(jQuery));
